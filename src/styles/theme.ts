12755d5fbbb87e01827bd3d527e75dfeceb13a79
GeonhyoPark815 <rjsgy0815@gmail.com>
2023-01-01 15:24:29 +0900

    [Feat] Save theme to use frequently

---
+++ src/styles/theme.ts
@@ -0,0 +1,22 @@
+const palette = {
+	WHITE: '#FFFFFF',
+	RED: '#D3623A',
+	EMERALD: '#6CB9B7',
+	PURPLE: '#625DA2',
+	GREY: '#7D7D7D',
+	BLACK: '#000000',
+};
+
+const size = {
+	mobile: '600px',
+	tablet: '900px',
+	laptop: '1200px',
+	desktop: '1800px',
+}
+
+const theme = {
+	palette,
+	size
+};
+
+export default theme;
