f2bbcab24ca7689ea95660a3de8fcab391f1bae6
GeonhyoPark815 <rjsgy0815@gmail.com>
2023-01-04 08:55:55 +0900

    [Feat] Add ReactRouterDom in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,23 +1,29 @@
-import {ThemeProvider} from "styled-components";
-import GlobalStyles from "../src/styles/GlobalStyles";
-import theme from "../src/styles/theme";
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../src/styles/GlobalStyles';
+import theme from '../src/styles/theme';
+import { MemoryRouter } from 'react-router-dom';
+import { addDecorator } from '@storybook/react';
 
 export const decorators = [
-  (Story) => (
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Story />
-      </ThemeProvider>
-  ),
+	(Story) => (
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<Story />
+		</ThemeProvider>
+	),
 ];
 
+addDecorator(
+    (story) =>
+    <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+);
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
