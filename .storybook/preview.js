7184f7dc081637f0a7bf52182046617c1145b049
GeonhyoPark815 <rjsgy0815@gmail.com>
2023-01-03 15:46:30 +0900

    [Feat] Apply Global Styles into Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,17 @@
+import {ThemeProvider} from "styled-components";
+import GlobalStyles from "../src/styles/GlobalStyles";
+import theme from "../src/styles/theme";
+
+export const decorators = [
+  (Story) => (
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Story />
+      </ThemeProvider>
+  ),
+];
+
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
